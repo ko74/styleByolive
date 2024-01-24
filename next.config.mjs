c8051c3276fd43c90d560a6bd9334227cfcfb27e
Adjei Francis <francisadjei801@gmail.com>
2024-01-24 09:16:46 +0000

    Structure done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Your custom configuration options go here
+  images: {
+    domains: ["cdn.shopify.com"], // Add the domains you want to allow for images
+  },
+};
 
 export default nextConfig;
